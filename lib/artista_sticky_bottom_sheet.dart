2cd1c51926ba59c0c5e3fc60e2f046df306bf036
ishaf <shafiulislam20@gmail.com>
2024-02-20 00:06:31 +0600

    first commit

---
+++ lib/artista_sticky_bottom_sheet.dart
@@ -0,0 +1,4 @@
+/// A Widget for sticky bottom sheet for scafold bottom sheet
+library artista_sticky_bottom_sheet;
+
+export 'src/artista_sticky_bottom_sheet.dart';
---
+++ test/src/artista_sticky_bottom_sheet_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:artista_sticky_bottom_sheet/artista_sticky_bottom_sheet.dart';
+
+void main() {
+  group('ArtistaStickyBottomSheet', () {
+    test('can be instantiated', () {
+
+    });
+  });
+}
